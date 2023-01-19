72b3407a09748a53560ea5e591de466a1e6aebba
ふぁ <yuki@yuki0311.com>
2023-01-19 13:51:04 +0900

    add config
    Signed-off-by: ふぁ <yuki@yuki0311.com>

---
+++ lib/constant/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  static String author = 'yuki';
+  static String year = '2022';
+  static String applicationLegalese = "$year $author";
+}
--- lib/constant/strings.dart
+++
@@ -1,5 +0,0 @@
-class TwitterStrings {
-  static String authority = "twitter.com";
-  static Uri home = Uri.https(authority, "/home");
-  static Uri login = Uri.https(authority, "/login");
-}
